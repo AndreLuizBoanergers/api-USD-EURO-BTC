bcfa8c0e976a5a42956fdde565bb14f2c930c476
Andre Luiz (Teddy) <97361918+AndreLuizBoanergers@users.noreply.github.com>
2023-01-22 23:50:51 -0300

    Update api.php
    Corecao linha 34 resultado BTC

--- api.php
+++ api.php
@@ -31,7 +31,7 @@ $vendaBTC = $json->BTCBRL->ask;
 
 $resultUSD = number_format($qtd * $compra,2,",","");
 $resultEURO = number_format($qtd * $compraEURO,2,",","");
-$resultBTC = $qtd/$compraBTC;
+$resultBTC = ($qtd/$compraBTC) / 100;
 
 $cotacoes = json_encode(array("REAL"=>array("Reais"=>$qtd, "Qtd Compra"=>$resultUSD, "Alta"=> $altaDollar, "Baixa"=>$baixaDollar,"Compra"=>$compra,"Venda"=>$venda,"Variacao"=>$variacao),"EURO"=>array("Reais"=>$qtd, "Qtd Compra"=>$resultEURO, "Alta"=> $altaEURO, "Baixa"=>$baixaEURO,"Compra"=>$compraEURO,"Venda"=>$vendaEURO,"Variacao"=>$variacaoEURO),"BTC"=>array("Reais"=>$qtd, "Qtd Compra"=>$resultBTC ,"Alta"=> $altaBTC, "Baixa"=>$baixaBTC,"Compra"=>$compraBTC,"Venda"=>$vendaBTC,"Variacao"=>$variacaoBTC)
 ));
